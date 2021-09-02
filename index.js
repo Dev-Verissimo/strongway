7de46ea6556858d235d3daf09e0f3c5debde174f
Dev <82881439+Dev-Verissimo@users.noreply.github.com>
2021-09-02 10:04:42 -0300

    pronto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let hamburger = document.getElementById("hamburger")
 let list = document.getElementsByClassName("header__item__list")[0]
-let ulTwo = document.getElementsByClassName('header__item__list--two')
+let ulTwo = document.getElementsByClassName('main__item__list')[0]
 let aside = document.getElementsByClassName('main__aside')[0]
 let open = false
 
@@ -9,10 +9,12 @@ hamburger.addEventListener('click' , () =>{
     if (open == false) {
         open = true
         aside.classList.add('menu')
-        ulTwo.style.display = "flex"
+        ulTwo.classList.add('main__item__list--two')
     }else{
         open = false
         aside.classList.remove('menu')
+        ulTwo.style.display = "none"
+        ulTwo.classList.remove('main__item__list--two')
     }
     
 })
